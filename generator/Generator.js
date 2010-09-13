e8d50d557aded03b7ee1d403317825dedf297eb4
Parashuram <n.parashuram@yahoo.co.in>
2010-09-13 23:06:22 +0530

    More refactoring

--- generator/Generator.js
+++ generator/Generator.js
@@ -1,7 +1,7 @@
 var Generator = (function(){
     var ajaxAll = function(urlList, allCallBack, options){
         var loaded = 0;
-        var result = {};
+        var result = [];
         if (urlList.length === 0) {
             allCallBack(result);
             return;
@@ -15,6 +15,7 @@ var Generator = (function(){
             $.extend(options, {
                 "url": urlList[loaded],
                 "complete": function(data, status, xhr){
+                    loaded++;
                     (typeof userCallbacks.complete === "function") && userCallbacks.complete(data, status, xhr);
                     if (loaded >= urlList.length && typeof allCallBack === "function") {
                         allCallBack.call(this, result);
@@ -24,7 +25,7 @@ var Generator = (function(){
                     }
                 },
                 "success": function(data, status, xhr){
-                    result[urlList[loaded++]] = data;
+                    result.push(data);
                     (typeof userCallbacks.success === "function") && userCallbacks.success(data, status, xhr);
                 }
             });
@@ -33,7 +34,6 @@ var Generator = (function(){
         ajaxOne();
     };
     
-    var templates = {};
     var fetchData = function(urls, callback, variable, ajaxOptions){
         var list = [];
         variable = variable || {};
@@ -44,50 +44,54 @@ var Generator = (function(){
         }
         ajaxAll(list, function(data){
             var result = {};
-            for (x in data) {
-                variable[x] = data[x];
+            for (var i = 0; i < data.length; i++) {
+                variable[list[i]] = data[i];
             }
-            var result = [];
-            for (var i = 0; i < urls.length; i++) {
-                result[urls[i]] = variable[urls[i]];
-            }
-            (typeof callback === "function") && callback(result);
+            callback(data);
         }, ajaxOptions);
     };
     
     return {
         "loadTemplates": function(urls, callback){
-            fetchData(urls, callback, this.templates);
+            this.templates = this.templates || {};
+            var me = this;
+            fetchData(urls, function(data){
+                var result = [];
+                for (var i = 0; i < urls.length; i++) {
+                    result.push(me.templates[urls[i]]);
+                }
+                callback(result);
+            }, this.templates);
         },
-        "loadData": function(urls, callback){
-            fetchData(urls, callback, this.values, {
+        "loadData": function(pageVariables, callback){
+            this.pageData = this.pageData || {};
+            var urls = [];
+            for (x in pageVariables) {
+                urls.push(pageVariables[x]);
+            }
+            var pageData = this.pageData;
+            fetchData(urls, function(data){
+                var result = {};
+                for (x in pageVariables) {
+                    result[x] = pageData[pageVariables[x]];
+                }
+                callback(result);
+            }, this.pageData, {
                 "dataType": "jsonp"
             });
         },
         "createPage": function(options, callback){
             var me = this;
             me.loadTemplates(options.templates, function(templates){
-                me.loadData(options.data, function(data){
-                    var result = [];
-                    for (var i = 0; i < options.templates.length; i++) {
-                        result.push(templates[options.templates[i]]);
+                me.loadData(options.data, function(pageData){
+                    var key = pageData;
+                    for (var i = 0; i < options.key.length; i++) {
+                        key = key[options.key[i]];
                     }
-                    
-                    try {
-                        var key = data[options.data];
-                        for (var i = 0; i < options.key.length; i++) {
-                            key = key[options.key[i]];
-                        }
-                        var content = $("<div>").append($.tmpl(result.join(""), key)).html();
-                        callback(content);
-                    } 
-                    catch (e) {
-                        console.error("Error", e);
-                    }
+                    var content = $("<div>").append($.tmpl(templates.join(""), key)).html();
+                    callback(content);
                 });
             });
-        },
-        "templates": {},
-        "values": {}
+        }
     }
 })();
